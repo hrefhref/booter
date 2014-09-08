e0b42b8906e924f7c37b21e02aa438c273773814
Jordan Bracco <self@jordan.io>
2014-09-08 15:05:21 +0200

    Package, v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,8 +3,10 @@ defmodule Booter.Mixfile do
 
   def project do
     [app: :booter,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0.0-rc1",
+     description: "Boot an Elixir application step by step",
+     package: package,
      deps: deps]
   end
 
@@ -17,5 +19,13 @@ defmodule Booter.Mixfile do
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.5", only: :dev},
    ]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Jordan Bracco"],
+      licenses: ["Mozilla Public License 1.1"],
+      links: %{"GitHub" => "https://github.com/eraserewind/booter",
+                "Docs" => "http://eraserewind.github.io/booter"}]
   end
 end
