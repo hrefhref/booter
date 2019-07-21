87d185049021a3af8b06b3cc08fc92e8bbe04cfe
Kristopher Bredemeier <k.bredemeier@gmail.com>
2019-07-22 00:20:09 +0200

    Configures inputs for formatter

--- .formatter.exs
+++ .formatter.exs
@@ -5,6 +5,7 @@ locals_without_parens = [
 ]
 
 [
+  inputs: ["mix.exs", "{config,lib.test}/**/*{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [
     locals_without_parens: locals_without_parens
