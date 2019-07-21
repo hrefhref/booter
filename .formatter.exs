e66593139eaff77f052a3905cb3ebf02fc4427a1
Kristopher Bredemeier <k.bredemeier@gmail.com>
2019-07-22 00:18:11 +0200

    Adds formatter config

---
+++ .formatter.exs
@@ -0,0 +1,12 @@
+locals_without_parens = [
+  boot_step: 1,
+  boot_step: 2,
+  boot_step: 3
+]
+
+[
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
+]
